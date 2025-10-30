6e3610b1d545ad8b18f44af759a1d147e40052ed
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2025-10-30 10:12:08 -0300

    Add __init__.py in classes

---
+++ Ecobot/classes/__init__.py
@@ -0,0 +1,14 @@
+from .chatbot import ChatBot
+from .chatbot_memory import ChatBotMemory
+from .chatbot_report import ChatbotReport
+from .chatbot_analytics import ChatbotAnalytics
+from .chatbot_frequent_questions import ChatbotFrequentQuestions
+
+# from classes import *
+__all__ = [
+    "ChatBot",
+    "ChatBotMemory",
+    "ChatbotReport",
+    "ChatbotAnalytics",
+    "ChatbotFrequentQuestions"
+]
